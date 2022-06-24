f915948597718b7fff8255d5e059c865ba9b178f
Anhtraihoa <20520839@gm.uit.edu.vn>
2022-06-24 18:17:40 +0700

    delete

---
+++ DoAn_QuanLyCuaHangThucPhamSach/DoAn_QuanLyCuaHangThucPhamSach/src/QuanLyCuaHang/DTO/TaiKhoan.java
@@ -0,0 +1,67 @@
+package QuanLyCuaHang.DTO;
+
+public class TaiKhoan {
+
+    private int maNhanVien;
+    private String tenDangNhap;
+    private String matKhau;
+    private String quyen;
+
+    public TaiKhoan() {
+    }
+
+    public TaiKhoan(int maNhanVien, String tenDangNhap, String matKhau, String quyen) {
+        this.maNhanVien = maNhanVien;
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
+        this.quyen = quyen;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public int getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    /**
+     *
+     * @param maNhanVien
+     */
+    public void setMaNhanVien(int maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    /**
+     *
+     * @param tenDangNhap
+     */
+    public void setTenDangNhap(String tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getQuyen() {
+        return quyen;
+    }
+
+    public void setQuyen(String quyen) {
+        this.quyen = quyen;
+    }
+}
